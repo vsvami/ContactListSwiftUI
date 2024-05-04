dbd044cc20ed632dae53f0571508617789ae9dab
vsvami <106614918+vsvami@users.noreply.github.com>
2024-05-04 21:45:34 +0400

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Vladimir Dmitriev on 04.05.24.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContactList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContactList
+//
+//  Created by Vladimir Dmitriev on 04.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
